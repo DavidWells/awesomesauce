1f3368e56065d107f84660560ee2e6a264b1bb1b
davidwells <david@inboundnow.com>
2018-06-01 14:43:31 -0700

    add custom function

---
+++ functions/custom-function.js
@@ -0,0 +1,10 @@
+/* Custom */
+exports.handler = function(event, context, callback) {
+  console.log('custom function')
+  console.log('event', event)
+  console.log('context', context)
+  return callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+  })
+}
