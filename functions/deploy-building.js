b1ac7a97550e1d071c632aef65d6dc903794459d
davidwells <david@inboundnow.com>
2018-06-07 18:30:24 -0700

    add json responses

--- functions/deploy-building.js
+++ functions/deploy-building.js
@@ -1,10 +1,12 @@
-
+/* Triggered when Netlify starts building a site for deployment. */
 exports.handler = function(event, context, callback) {
   console.log('deploy-building')
   console.log('event', event)
   console.log('context', context)
   return callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: JSON.stringify({
+      data: true
+    })
   })
 }
