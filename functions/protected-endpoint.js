df0c13b20279cd42ba4bf112b642ed66b6949acb
DavidWells <hello@davidwells.io>
2018-10-01 17:13:40 -0700

    update protected endpoint

--- functions/protected-endpoint.js
+++ functions/protected-endpoint.js
@@ -1,28 +1,3 @@
-import jwt from 'jsonwebtoken'
-
-function checkAuth(authToken) {
-  return new Promise((resolve, reject) => {
-    // Now Verify the jwt token is valid
-    try {
-      jwt.verify(authToken, process.env.SECRET, {}, (verifyError, decoded) => {
-        if (verifyError) {
-          console.log('Token signature NOT VERIFIED', verifyError)
-          return reject(new Error('Token signature NOT VERIFIED'))
-        }
-        // output for logs
-        console.log('------------------')
-        console.log('decoded jwt token')
-        console.log(decoded)
-        console.log('------------------')
-        // Everything is ok!
-        return resolve(decoded)
-      })
-    } catch (err) {
-      console.log('jwt.verify exception', err)
-      return reject(err)
-    }
-  })
-}
 
 /* Protected function */
 exports.handler = function(event, context, callback) {
@@ -30,6 +5,7 @@ exports.handler = function(event, context, callback) {
   const claims = context.clientContext && context.clientContext.user
   console.log('claims', claims)
   if (!claims) {
+    console.log('No claims! Begone!')
     return callback(null, {
       statusCode: 401,
       body: JSON.stringify({
@@ -37,23 +13,11 @@ exports.handler = function(event, context, callback) {
       })
     })
   }
-  // Use the event data auth header to verify
-  checkAuth(event).then((user) => {
-    return callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({
-        data: 'SECRET STUFFFFFFFFFFFFFF OOOOOOOooooooooooo'
-      })
-    })
-  }).catch((errorMsg) => {
-    console.log('errorMsg', errorMsg)
-    console.log(typeof errorMsg)
-    // return error back to app
-    return callback(null, {
-      statusCode: 401,
-      body: JSON.stringify({
-        error: errorMsg,
-      })
+  console.log('User is allowed. Let them in')
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: 'SECRET STUFFFFFFFFFFFFFF OOOOOOOooooooooooo'
     })
   })
 }
